2814d0c2351fc6c99b787d09540a1489a2a082ca
merlose <37470789+merlose@users.noreply.github.com>
2019-05-05 15:26:53 +0100

    update simplified variable names

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -1,6 +1,8 @@
 # menu. accepts user input until "quit" is used.
 # a while loop is used for the menu
 
+
+
 while True:
     print("Options:")
     print("Enter + to add two numbers")
@@ -13,24 +15,24 @@ while True:
     if user_input == "Get me out of here":
         break
     elif user_input == "+":
-        add1 = float(input("Add: "))
-        add2 = float(input("to: "))
-        result = str(add1 + add2)
+        num1 = float(input("Add: "))
+        num2 = float(input("to: "))
+        result = str(num1 + num2)
         print ("= " + result)
     elif user_input == "-":
-        sub1 = float(input("Subtract: "))
-        sub2 = float(input("by: "))
-        result = str(sub1 - sub2)
+        num1 = float(input("Subtract: "))
+        num2 = float(input("by: "))
+        result = str(num1 - num2)
         print ("= " + result)
     elif user_input == "*":
-        multi1 = float(input("Multiply: "))
-        multi2 = float(input("with: "))
-        result = str(multi1 * multi2)
+        num1 = float(input("Multiply: "))
+        num2 = float(input("with: "))
+        result = str(num1 * num2)
         print ("= " + result)
     elif user_input == "/":
-        div1 = float(input("Divide: "))
-        div2 = float(input("by: "))
-        result = str(div1 / div2)
+        num1 = float(input("Divide: "))
+        num2 = float(input("by: "))
+        result = str(num1 / num2)
         print ("= " + result)
     else:
         print ("Unknown input")
